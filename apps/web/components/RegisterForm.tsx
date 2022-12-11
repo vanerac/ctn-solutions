2be2ec046fb6458bb0be2e15b0152c3a09c026e5
Corentin MAS <corentinmas@yahoo.com>
2022-12-11 18:15:36 +1100

    [feature/auth] Updated Login & Register form using tailwind

--- apps/web/components/RegisterForm.tsx
+++ apps/web/components/RegisterForm.tsx
@@ -25,27 +25,46 @@ export default function RegisterForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Email
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </label>
-            <button type="submit" disabled={loading}>
-                {loading ? 'Loading...' : 'Register'}
-            </button>
-            {error && <p style={{color: 'red'}}>{error}</p>}
-        </form>
+        <div className="flex items-center justify-center h-screen">
+            <form onSubmit={handleSubmit} className="w-1/3 p-8 bg-white border rounded shadow">
+                <h1 className="text-3xl font-bold mb-6 text-center">Register</h1>
+                <div className="mb-4">
+                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="******************"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {error && <p className="text-xs italic text-red-500">{error}</p>}
+                </div>
+                <div className="mb-6 text-center">
+                    <button
+                        className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                        type="submit"
+                        disabled={loading}
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 }
--- apps/web/pages/register.tsx
+++ apps/web/pages/register.tsx
@@ -3,7 +3,6 @@ import RegisterForm from "../components/RegisterForm";
 export default function Register() {
     return (
         <div>
-            <h1>Register</h1>
             <RegisterForm/>
         </div>
     )
