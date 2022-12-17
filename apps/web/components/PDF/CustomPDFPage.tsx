28260f533358b7e23210b54177885b6d15b66323
Corentin MAS <corentinmas@yahoo.com>
2022-12-17 20:05:55 +1100

    [feature/pdf-viewer] Added signature placeholder

--- apps/web/components/PDF/CustomPDFPage.tsx
+++ apps/web/components/PDF/CustomPDFPage.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {PDFPageProxy} from "pdfjs-dist";
 
 export default function CustomPDFPage({page, parentContainer, options}: {
@@ -7,6 +7,7 @@ export default function CustomPDFPage({page, parentContainer, options}: {
     options?: Object
 }) {
     const canvasRef = useRef<any>(null);
+    const [viewport, setViewport] = useState<any>(null);
 
     console.log(page);
 
@@ -23,8 +24,27 @@ export default function CustomPDFPage({page, parentContainer, options}: {
         const renderContext = {canvasContext: ctx, viewport};
 
         page.render(renderContext);
+        setViewport(viewport);
     }, [])
 
 
-    return <canvas ref={canvasRef} style={{height: '100vh'}}/>;
+    return (
+        <div style={{position: "relative"}}>
+            <canvas ref={canvasRef} style={{height: '100vh'}}/>
+            {/*TODO: Add signature module*/}
+            <div style={{
+                position: "absolute",
+                top: (viewport?.height ?? 0) / 2,
+                left: (viewport?.width ?? 0) / 2,
+                width: '25%',
+                height: '8%',
+                backgroundColor: 'rgba(0,0,0,0.5)'
+            }}>
+                <button
+                    onClick={() => alert('Hello')}
+                >Sign
+                </button>
+            </div>
+        </div>
+    );
 }
